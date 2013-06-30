ceb9fc31a2bd98c47838dc96b93151dbe2c2da20
Zeg9 <dazeg9@gmail.com>
2013-06-30 14:38:48 +0200

    Better linking (but keep compatible with older versions)

--- teleporters/init.lua
+++ teleporters/init.lua
@@ -44,46 +44,8 @@ teleporters.find_safe = function (_pos)
 	return _pos
 end
 
-teleporters.network = {}
 
-teleporters.file = minetest.get_worldpath()..'/teleporters'
-
-teleporters.save = function()
-	local output = ''
-	for id, coords in pairs(teleporters.network) do output = output..id..':'..coords.x..','..coords.y..','..coords.z..';'	end
-	local f = io.open(teleporters.file, "w")
-	f:write(output)
-	io.close(f)
-end
-
-teleporters.load = function()
-	local f = io.open(teleporters.file, "r")  
-	if f then
-		local contents = f:read()
-		io.close(f)
-		if contents ~= nil then 
-			local entries = contents:split(";") 
-			for i,entry in pairs(entries) do
-				local id, coords = unpack(entry:split(":"))
-				local p = {}
-				p.x, p.y, p.z = string.match(coords, "^([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+)$")
-				if p.x and p.y and p.z then
-					teleporters.network[tonumber(id)] = {x = tonumber(p.x),y= tonumber(p.y),z = tonumber(p.z)}
-				end
-			end
-		end
-	end
-end
-
-teleporters.load()
-
-teleporters.get_new_id = function()
-	id = 0
-	for k,_ in pairs(teleporters.network) do
-		if k > id then id = k end
-	end
-	return id+1
-end
+dofile(minetest.get_modpath("teleporters").."/legacy.lua")
 
 teleporters.make_formspec = function (meta)
 	formspec = "size[6,3]" ..
@@ -102,6 +64,9 @@ teleporters.reset_cooldown = function (params)
 	teleporters.is_teleporting[params.playername] = false
 end
 
+teleporters.selected = {}
+-- teleporters.selected[player_name] = pos
+
 -- Nodes and items
 
 minetest.register_node("teleporters:teleporter", {
@@ -115,14 +80,19 @@ minetest.register_node("teleporters:teleporter", {
 	groups = {cracky=1},
 	sounds = default.node_sound_stone_defaults(),
 	light_source = 10,
-	on_construct = function(pos)
-		id = teleporters.get_new_id()
-		teleporters.network[id] = pos
-		teleporters.save()
-		local meta = minetest.env:get_meta(pos)
-		meta:set_int("id",id)
-		meta:set_string("infotext","Teleporter")
-		meta:set_string("formspec",teleporters.make_formspec(meta))
+	after_place_node = function(pos, placer, itemstack)
+		local meta = minetest.get_meta(pos)
+		local name = placer:get_player_name()
+		if teleporters.selected[name] then
+			-- link teleporters
+			local target = teleporters.selected[name]
+			meta:set_string("target",minetest.pos_to_string(target))
+			local target_meta = minetest.get_meta(target)
+			target_meta:set_string("target",minetest.pos_to_string(pos))
+			teleporters.selected[name] = nil
+		else
+			teleporters.selected[name] = pos
+		end
 	end,
 	on_receive_fields = function(pos, formname, fields, sender)
 		if fields.desc then
@@ -143,25 +113,22 @@ teleporters.use_teleporter = function(obj,pos)
 		teleporters.is_teleporting[obj:get_player_name()] = true
 	end
 	local meta = minetest.env:get_meta(pos)
-	if false then -- TODO make a better way to link them (I know how to, just feel lazy today)
-	else -- Backward compatibility with older versions
-		minetest.sound_play("teleporters_teleport",{pos=pos,gain=1,max_hear_distance=32})
-		if meta:get_int("id") %2 == 0 then newpos = teleporters.network[meta:get_int("id")-1]
-		else newpos = teleporters.network[meta:get_int("id")+1] end
-		if not newpos then newpos = pos end
-		newpos = teleporters.copy_pos(newpos)
-		newpos = teleporters.find_safe(newpos)
-		if obj:is_player() then
-			minetest.sound_play("teleporters_teleport",{gain=1,to_player=obj:get_player_name()})
-		end
-		newpos.y = newpos.y + .5
-		newpos.y = newpos.y -1
-		teleporters.teleport({obj=obj,target=newpos})
-		newpos.y = newpos.y +1
-		minetest.after(.1, teleporters.teleport, {obj=obj,target=newpos})
-		if obj:is_player() then
-			minetest.after(PLAYER_COOLDOWN, teleporters.reset_cooldown, {playername=obj:get_player_name()})
-		end
+	local target = pos
+	if meta:get_string("target") ~= "" then
+		target = minetest.string_to_pos(meta:get_string("target"))
+	elseif meta:get_stirng("id") > 0 then -- Compatibility with older versions
+		if meta:get_int("id") %2 == 0 then target = teleporters.network[meta:get_int("id")-1]
+		else target = teleporters.network[meta:get_int("id")+1] end
+	end
+	
+	local newpos = teleporters.find_safe(teleporters.copy_pos(target))
+	if obj:is_player() then
+		minetest.sound_play("teleporters_teleport",{gain=1,to_player=obj:get_player_name()})
+	end
+	newpos.y = newpos.y + .5
+	teleporters.teleport({obj=obj,target=newpos}) -- TODO: particles and change player yaw
+	if obj:is_player() then
+		minetest.after(PLAYER_COOLDOWN, teleporters.reset_cooldown, {playername=obj:get_player_name()})
 	end
 end
 
@@ -187,9 +154,7 @@ minetest.register_globalstep(function(dtime)
 		pos = player:getpos()
 		pos = {x=math.floor(pos.x+.5),y=math.floor(pos.y),z=math.floor(pos.z+.5)}
 		if minetest.env:get_node(pos).name == "teleporters:teleporter" then
-			if minetest.env:get_meta(pos):get_int("id") > 0 then
-				teleporters.use_teleporter(player,pos)
-			end
+			teleporters.use_teleporter(player,pos)
 		end
 	end
 end)
